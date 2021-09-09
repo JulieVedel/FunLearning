5fdeb5ff41bb68ef691e0736c5392e95f28534ad
JulieVedel <juli76k0@edu.easj.dk>
2021-09-09 13:08:02 +0200

    Weekdays 2

--- src/com/company/Weekdays2.java
+++ src/com/company/Weekdays2.java
@@ -6,14 +6,14 @@ public class Weekdays2 {
         String[] weekdays = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
         for (int i = 0; i < weekdays.length; i++) {
-            
+
             if (weekdays[i] == "Sunday" || weekdays[i] == "Saturday") {
               System.out.println("Today it is " + weekdays[i] + "! And it's a free day!");
-            }
-
-            else {
+            } else {
                 System.out.println("Today it is " + weekdays[i] + "!");
             }
+
+
         }
     }
 }
