dad8f1bef3572f8da1c1f13545e6a52bb1f571cc
JulieVedel <juli76k0@edu.easj.dk>
2021-09-27 13:35:25 +0200

    Changes

--- src/com/company/Fibonacci.java
+++ src/com/company/Fibonacci.java
@@ -3,12 +3,14 @@ package com.company;
 public class Fibonacci {
 
     public static void main(String[] args) {
-        fibonacci(10);
+        int n = 10;
+        System.out.print("The fibonacci value to n = " + n + " is " + fibonacci(n));
+        
     }
-    public static void fibonacci(int loops) {
+    public static int fibonacci(int loops) {
         int first = 1;
         int second = 0;
-        int sum;
+        int sum = 0;
         System.out.println("n = 0 and nx = 0");
         for (int n = 1; n <= loops; n++) {
             sum = first + second;
@@ -16,5 +18,6 @@ public class Fibonacci {
             second = sum;
             System.out.println("n = " + n + " and nx = " + sum);
         }
+        return sum;
     }
 }
